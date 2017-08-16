2d217e81194093a05b00994326673a5accfb0b65
croatproject <croatproject@gmail.com>
2017-08-16 11:42:06 +0200

    Fix discovered by Monero Lab implemented

--- src/CryptoNoteCore/Core.cpp
+++ src/CryptoNoteCore/Core.cpp
@@ -279,11 +279,40 @@ bool core::check_tx_semantic(const Transaction& tx, bool keeped_by_block) {
 }
 
 bool core::check_tx_inputs_keyimages_diff(const Transaction& tx) {
+
+  // parameters used for the additional key_image check
+  static const Crypto::KeyImage Z = { { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 } };
+  static const Crypto::KeyImage I = { { 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 } };
+  static const Crypto::KeyImage L = { { 0xed, 0xd3, 0xf5, 0x5c, 0x1a, 0x63, 0x12, 0x58, 0xd6, 0x9c, 0xf7, 0xa2, 0xde, 0xf9, 0xde, 0x14, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10 } };
+
   std::unordered_set<Crypto::KeyImage> ki;
-  for (const auto& in : tx.inputs) {
-    if (in.type() == typeid(KeyInput)) {
-      if (!ki.insert(boost::get<KeyInput>(in).keyImage).second)
+  std::set<std::pair<uint64_t, uint32_t>> outputsUsage;
+  for (const auto& input : tx.inputs) {
+    if (input.type() == typeid(KeyInput)) {
+      const KeyInput& in = boost::get<KeyInput>(input);
+      if (!ki.insert(in.keyImage).second) {
+        logger(ERROR) << "Transaction has identical key images";
+          return false;
+      }
+
+      if (in.outputIndexes.empty()) {
+        logger(ERROR) << "Transaction's input uses empty output";
         return false;
+      }
+
+	  // additional key_image check
+	  // Fix discovered by Monero Lab and suggested by "fluffypony" (bitcointalk.org)
+	  if (!(scalarmultKey(in.keyImage, L) == I)) {
+		  logger(ERROR) << "Transaction uses key image not in the valid domain";
+		  return false;
+	  }
+
+      // outputIndexes are packed here, first is absolute, others are offsets to previous,
+      // so first can be zero, others can't
+      if (std::find(++std::begin(in.outputIndexes), std::end(in.outputIndexes), 0) != std::end(in.outputIndexes)) {
+        logger(ERROR) << "Transaction has identical output indexes";
+        return false;
+      }
     }
   }
   return true;
